ec3239040efde65f8f0be90a6761137f07c85c50
Aharon86 <aharonhg@gmail.com>
2019-04-17 22:48:12 +0400

    add menu

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,16 +1,26 @@
 import React,{Component} from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { FaUsers } from "react-icons/fa";
+import { IoIosSend } from "react-icons/io";
 
 class Menu extends Component{
     render(){
         return (
             <div id="mainMenu">
-                <div>
-                    <Link to="/mailinglist">mailinglist</Link>                    
+                <div className = 'itemMenu contacts'>
+                    <Link to="/contacts">
+                        <FaUsers />
+                        <span>Contacts</span>
+                    </Link>                    
                 </div>
-                 <div>
-                    <Link to="/contacts">contacts</Link>                    
+                <div className = 'itemMenu mailingList'>
+                    <Link to="/mailinglist">
+                        <IoIosSend /> 
+                        <span>Mailinglist</span>
+                    </Link>                    
                 </div>
+                 
+                
             </div>
         )
     }
