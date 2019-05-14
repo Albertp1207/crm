16785c5312c25e2ae9ab5311381f4bd67a28d7c0
Albert <albertp1207@mail.ru>
2019-05-14 16:24:30 +0400

    Updates

---
+++ precache-manifest.7c34be3724c5a3ed4bc28ba1318f0bfa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ddaf5e807cf1b3974259",
+    "url": "/crm/static/css/main.73485113.chunk.css"
+  },
+  {
+    "revision": "ddaf5e807cf1b3974259",
+    "url": "/crm/static/js/main.5bac1d64.chunk.js"
+  },
+  {
+    "revision": "74d3d11ddd56d6450b30",
+    "url": "/crm/static/js/runtime~main.2ce2ed3f.js"
+  },
+  {
+    "revision": "4a70e0241a5814b09e0f",
+    "url": "/crm/static/js/2.899df949.chunk.js"
+  },
+  {
+    "revision": "27f6a149ff2294ce9f3c2e8cde8e70b5",
+    "url": "/crm/static/media/logo32.27f6a149.png"
+  },
+  {
+    "revision": "9b4981fef7fc0a1da79a53e8c79dbd18",
+    "url": "/crm/static/media/Orbitron-Medium.9b4981fe.ttf"
+  },
+  {
+    "revision": "b0bdce0221038e38d760dd1cc5cfe8e3",
+    "url": "/crm/static/media/Background-CRM.b0bdce02.png"
+  },
+  {
+    "revision": "0f9054a55f33db69f76eba9592e6e338",
+    "url": "/crm/index.html"
+  }
+];
